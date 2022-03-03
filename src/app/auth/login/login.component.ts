3d36d5383b572aa48b0540ca27bbac323732d9ac
olcay <olcaymaster@gmail.com>
2022-03-03 04:46:05 +0300

    small fixes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -32,7 +32,7 @@ export class LoginComponent implements OnInit {
     this.form = formBuilder.group(
       {
         email: [null, [Validators.required, Validators.email]],
-        password: [null, [Validators.required, Validators.min(8)]]
+        password: ['123456', [Validators.required, Validators.min(8)]]
       },
       {
         updateOn: 'submit'
@@ -43,39 +43,43 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {}
 
   submit(): void {
-    if (this.form.valid) {
-      this.submitted = true;
+    this.submitted = true;
 
-      // Get the form values
-      const { email, password } = this.form.value;
+    // Get the form values
+    const { email, password } = this.form.value;
 
-      this.authService
-        .login(email, password)
-        .pipe(
-          catchError((err, caught) => {
-            this.form.get('email')!.setErrors({
-              emailNotFound: true
-            });
-            markDirty(this);
-            this.submitted = false;
-            return EMPTY;
-          })
-        )
-        .subscribe((value) => {
+    this.authService
+      .login(email, password)
+      .pipe(
+        catchError((err, caught) => {
+          console.error(err)
+          this.form.get('email')!.setErrors({
+            emailNotFound: true
+          });
+          markDirty(this);
           this.submitted = false;
-          if (value.error) {
-            alert('error');
-            console.log(value);
-          } else {
-            // Redirect the user
-            this.router.navigate([this.authService.redirectUrl]).then((value1) => {
-              // Kullanicinin favori etiketlerini kaydet
-              this.tagService.getFavoriteTags().subscribe((value2) => {
-                value2 && localStorage.setItem('watchedTags', JSON.stringify(value2));
-              });
-            });
-          }
-        });
-    }
+          return EMPTY;
+        })
+      )
+      .subscribe((value) => {
+        this.submitted = false;
+        if (value.error) {
+          console.log(value);
+        } else {
+          // Redirect the user
+          this.router.navigate([this.authService.redirectUrl]).then((value1) => {
+            this.saveFavoriteTagsToLocaleStorage();
+          });
+        }
+      });
+  }
+
+  /**
+   * Save favorite tags to local storage
+   */
+  saveFavoriteTagsToLocaleStorage() {
+    this.tagService.getFavoriteTags().subscribe((value2) => {
+      value2 && localStorage.setItem('watchedTags', JSON.stringify(value2));
+    });
   }
 }
