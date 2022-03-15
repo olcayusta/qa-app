3a6320444ecffd6b86a3dfab7bcf198687cc25a8
olcay <olcayusta02@gmail.com>
2022-03-15 23:01:11 +0300

    AvatarButton ve NotificationButton modul olarak olusturuldugu icin, gereksiz kod silindi.

--- src/app/main-layout/main-layout.module.ts
+++ src/app/main-layout/main-layout.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AvatarButtonComponent } from '../main/components/top-bar/components/avatar-button/avatar-button.component';
-import { NotificationButtonComponent } from '../main/components/top-bar/components/notification-button/notification-button.component';
 import { MaterialModule } from '@modules/material/material.module';
 import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [AvatarButtonComponent, NotificationButtonComponent],
-  imports: [CommonModule, MaterialModule, SharedModule],
-  exports: [AvatarButtonComponent, NotificationButtonComponent]
+  imports: [CommonModule, MaterialModule, SharedModule]
 })
 export class MainLayoutModule {}
