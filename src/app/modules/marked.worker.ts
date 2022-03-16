32e996f1be066e1882770c50bf33eceb86fb1763
olcay <olcayusta02@gmail.com>
2022-03-16 23:09:55 +0300

    marked.worker.ts silindi

--- src/app/modules/marked.worker.ts
+++
@@ -1,8 +0,0 @@
-/// <reference lib="webworker" />
-
-import { marked } from 'marked';
-
-addEventListener('message', ({ data }) => {
-  const response = marked.parse(data);
-  postMessage(response);
-});
