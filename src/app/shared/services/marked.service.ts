058f55c3d8dbffe858b9257b8143f29b05396699
olcay <olcayusta02@gmail.com>
2022-03-16 16:32:50 +0300

    marked.service.ts guncellendi

--- src/app/shared/services/marked.service.ts
+++ src/app/shared/services/marked.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
-import { marked } from 'marked';
+import { Observable } from 'rxjs';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class MarkedService {
-  constructor() {}
+  worker: Worker = new Worker(new URL('../tasty.worker', import.meta.url));
 
-  setMarked(text: string): void {
+  constructor(private domSanitizer: DomSanitizer) {}
+
+  workerMessages(): Observable<SafeHtml> {
+    return new Observable((subscriber) => {
+      this.worker.onmessage = ({ data }) => {
+        subscriber.next(this.domSanitizer.bypassSecurityTrustHtml(data));
+      };
+      this.worker.onerror = (err) => {
+        subscriber.error(err);
+      };
+    });
+  }
+
+  /*  setMarked(text: string): void {
     marked.parse(text, (error, parseResult) => {
       console.log(parseResult);
     });
-  }
+  }*/
 }
