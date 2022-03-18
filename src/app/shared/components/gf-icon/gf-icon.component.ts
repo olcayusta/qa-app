5b02260f871c70dbaadfd282c6b350a14dc4ced6
olcay <olcayusta02@gmail.com>
2022-03-19 00:32:44 +0300

    Bilesen iyilestirmeleri

--- src/app/shared/components/gf-icon/gf-icon.component.ts
+++ src/app/shared/components/gf-icon/gf-icon.component.ts
@@ -1,14 +1,10 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gf-icon',
   templateUrl: `./gf-icon.component.html`,
   styleUrls: ['./gf-icon.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GfIconComponent {
   @Input() name!: string;
