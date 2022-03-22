f421f76d548cabf74574ccb5c1a6c7c5074d5aae
olcay <olcayusta02@gmail.com>
2022-03-22 21:58:36 +0300

    SSE baglantisi sabit olarak eklendi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:9001',
+  apiUrl: 'http://192.168.1.4:9001',
   appTitle: 'QaApp',
   PUBLIC_VAPID_KEY_OF_SERVER:
     'BBuui5T3pIWV_ZyyZel8ynOQLlJO5juM6knPoBzg9shZjkmcqaFm0okyCbWOw2pxSSdEo2p9kiSQYn0V8A-GOjE',
-  WS_URL: 'ws://localhost:9001/notification'
+  WS_URL: 'ws://192.168.1.4:9001/notification',
+  SSE_URL: '//loalhost:9001/stream'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,8 @@ export const environment = {
   appTitle: 'QaApp',
   PUBLIC_VAPID_KEY_OF_SERVER:
     'BBuui5T3pIWV_ZyyZel8ynOQLlJO5juM6knPoBzg9shZjkmcqaFm0okyCbWOw2pxSSdEo2p9kiSQYn0V8A-GOjE',
-  WS_URL: 'ws://localhost:9001/notification'
+  WS_URL: 'ws://localhost:9001/notification',
+  SSE_URL: '//loalhost:9001/stream'
 };
 
 /*
