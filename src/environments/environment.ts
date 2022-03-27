3c8ed4af15c78c070266d813e66f38a29e31f8d5
olcay <olcayusta02@gmail.com>
2022-03-27 15:15:41 +0300

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   PUBLIC_VAPID_KEY_OF_SERVER:
     'BBuui5T3pIWV_ZyyZel8ynOQLlJO5juM6knPoBzg9shZjkmcqaFm0okyCbWOw2pxSSdEo2p9kiSQYn0V8A-GOjE',
   WS_URL: 'ws://localhost:9001/notification',
-  SSE_URL: '//loalhost:9001/stream'
+  SSE_URL: '//localhost:9001/stream'
 };
 
 /*
