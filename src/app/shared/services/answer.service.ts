6b020a7e158a076f4c2cf67cef7d3686c1e3618c
olcay <olcayusta02@gmail.com>
2022-04-05 19:36:18 +0300

    update answer.service.ts

--- src/app/shared/services/answer.service.ts
+++ src/app/shared/services/answer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { Answer } from '../models/answer.model';
 import { Observable } from 'rxjs';
@@ -8,10 +8,18 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AnswerService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   getAnswers(questionId: number): Observable<Answer[]> {
     return this.http.get<Answer[]>(`${environment.apiUrl}/questions/${questionId}/answers`);
+  }
+
+  getAnswersBySort(questionId: number, sortBy: string): Observable<Answer[]> {
+    const params = new HttpParams().set('sort_by', sortBy);
+    return this.http.get<Answer[]>(`${environment.apiUrl}/questions/${questionId}/answers`, {
+      params
+    });
   }
 
   create(questionId: number, content: string): Observable<Answer> {
