acd20541beb652d5ad5a6595f498d8ad7346fd28
olcay <olcayusta02@gmail.com>
2022-10-04 16:15:25 +0300

    more refactor...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
 import { AuthGuard } from '@auth/auth.guard';
 import { LoggedGuard } from '@auth/logged.guard';
-import { watchedTagsRoutes } from './watched-tags/watched-tags.routes';
 import { settingsRoutes } from './settings/settings.routes';
 import { searchRoutes } from './search/search.routes';
-import { tagRoutes } from './tag/tag.routes';
 import { tagsRoutes } from './tags/tags.routes';
-import { usersRoutes } from './users/users.routes';
 import { homeRoutes } from './home/home.routes';
 import { createQuestionRoutes } from './create-question/create-question.routes';
 import { pageNotFoundRoutes } from './page-not-found/page-not-found-routes';
@@ -24,7 +20,7 @@ export const appRoutes: Routes = [
       },
       {
         path: 'users',
-        loadChildren: () => import('./users/users.routes').then(({ usersRoutes }) => usersRoutes)
+        loadChildren: () => import('./users/users.routes').then(c => c.USERS_ROUTES)
       },
       {
         path: 'tags',
@@ -32,7 +28,7 @@ export const appRoutes: Routes = [
       },
       {
         path: 'tag/:tagId',
-        loadChildren: () => import('./tag/tag.routes').then(({ tagRoutes }) => tagRoutes)
+        loadChildren: () => import('./tag/tag.routes').then(c => c.TAG_ROUTES)
       },
       {
         path: 'search',
@@ -45,16 +41,15 @@ export const appRoutes: Routes = [
       },
       {
         path: 'list',
-        loadChildren: () => import('./list/list.routes').then(({ listRoutes }) => listRoutes)
+        loadChildren: () => import('./list/list.routes').then(c => c.LIST_ROUTES)
       },
       {
         path: 'watched_tags',
-        loadChildren: () =>
-          import('./watched-tags/watched-tags.routes').then(({ watchedTagsRoutes }) => watchedTagsRoutes)
+        loadChildren: () => import('./watched-tags/watched-tags.routes').then(c => c.WATCHED_TAG_ROUTES)
       },
       {
         path: 'user/:userId',
-        loadChildren: () => import('./user/user.routes').then(({ userRoutes }) => userRoutes)
+        loadChildren: () => import('./user/user.routes').then(c => c.USER_ROUTES)
       },
       {
         path: 'question/:questionId',
