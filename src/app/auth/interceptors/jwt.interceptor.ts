ed75ef4ca0857d3b28e92c353b87da16d9afba23
olcay <olcayusta02@gmail.com>
2022-10-14 20:29:55 +0300

    fixes

--- src/app/auth/interceptors/jwt.interceptor.ts
+++ src/app/auth/interceptors/jwt.interceptor.ts
@@ -1,11 +1,6 @@
-import { inject, Injectable } from '@angular/core';
+import { inject } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HttpContextToken,
-  HttpHandlerFn
+  HttpRequest, HttpEvent, HttpHandlerFn
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
@@ -13,6 +8,10 @@ import { environment } from '@environments/environment';
 
 export function jwtInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   const authService = inject(AuthService);
+
+  /*    if (req.context.get(BYPASS_LOG)) {
+  return next.handle(req);
+}*/
 
   const currentUser = authService.userValue;
   const isLoggedIn = currentUser && currentUser.token;
@@ -26,30 +25,4 @@ export function jwtInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): 
   }
 
   return next(req);
-}
-
-
-@Injectable()
-export class JwtInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {
-  }
-
-  intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    /*    if (req.context.get(BYPASS_LOG)) {
-      return next.handle(req);
-    }*/
-
-    const currentUser = this.authService.userValue;
-    const isLoggedIn = currentUser && currentUser.token;
-    const isApiUrl = req.url.startsWith(environment.apiUrl);
-    if (isLoggedIn && isApiUrl) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${currentUser.token}`
-        }
-      });
-    }
-
-    return next.handle(req);
-  }
 }
--- src/app/core/interceptors/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -1,9 +1,7 @@
-import { inject, Injectable } from '@angular/core';
+import { inject } from '@angular/core';
 import {
   HttpRequest,
-  HttpHandler,
   HttpEvent,
-  HttpInterceptor,
   HttpErrorResponse,
   HttpContextToken, HttpHandlerFn
 } from '@angular/common/http';
@@ -12,37 +10,6 @@ import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 export const BYPASS_ERROR = new HttpContextToken(() => false);
-
-@Injectable()
-export class HttpErrorInterceptor implements HttpInterceptor {
-  private router = inject(Router);
-
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (request.context.get(BYPASS_ERROR)) {
-      return next.handle(request);
-    }
-
-    return next.handle(request).pipe(
-      catchError((error: HttpErrorResponse) => {
-        if (error.status === 404) {
-          this.router.navigateByUrl('404', {
-            replaceUrl: true,
-            skipLocationChange: true
-          });
-        }
-
-        if (error.status === 500) {
-          this.router.navigateByUrl('500', {
-            replaceUrl: true,
-            skipLocationChange: true
-          });
-        }
-        return EMPTY;
-      })
-    );
-  }
-}
 
 export function errorInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   const router = inject(Router);
